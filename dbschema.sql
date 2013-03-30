8c845e47de6a191803a592e79c0b40abac5e9028
Manoj Dhoble <manoj.dhoble@gmail.com>
2013-03-30 14:35:59 -0700

    Working DB Schema file

--- dbschema.sql
+++ dbschema.sql
@@ -1,20 +1,23 @@
+DROP OWNED BY cmpe275 CASCADE;
 DROP ROLE IF EXISTS cmpe275;
 CREATE ROLE cmpe275 LOGIN ENCRYPTED PASSWORD 'md5aa1efa18e98899a187795f467b4b3d8d'
   SUPERUSER CREATEDB CREATEROLE
    VALID UNTIL '2015-03-30 00:00:00';
 
 
-DROP DATABASE IF EXISTS cmpe275
+DROP DATABASE IF EXISTS cmpe275;
 CREATE DATABASE cmpe275
   WITH ENCODING='UTF8'
        OWNER=cmpe275
        CONNECTION LIMIT=-1;
    
+\c cmpe275;
+
 DROP SCHEMA IF EXISTS site1 CASCADE;   
 CREATE SCHEMA site1
        AUTHORIZATION cmpe275;
 
-DROP TABLE IF EXISTS site1.userdata CASCADE       
+DROP TABLE IF EXISTS site1.userdata CASCADE;      
 CREATE TABLE site1.userdata
 (
    user_id character varying(128) NOT NULL, 
@@ -22,7 +25,7 @@ CREATE TABLE site1.userdata
    city character varying(128) NOT NULL, 
    zip_code character varying(128) NOT NULL, 
    password character varying(32) NOT NULL, 
-   CONSTRAINT PRIMARY KEY (user_id)
+   PRIMARY KEY (user_id)
 ) 
 WITH (
   OIDS = FALSE
@@ -30,7 +33,7 @@ WITH (
 ALTER TABLE site1.userdata
   OWNER TO cmpe275;
   
-DROP TABLE IF EXISTS site1.image CASCADE    
+DROP TABLE IF EXISTS site1.image CASCADE; 
 CREATE TABLE site1.image
 (
    id serial NOT NULL, 
@@ -53,7 +56,7 @@ DROP SCHEMA IF EXISTS site2 CASCADE;
 CREATE SCHEMA site2
        AUTHORIZATION cmpe275;
        
-DROP TABLE IF EXISTS site2.userdata CASCADE       
+DROP TABLE IF EXISTS site2.userdata CASCADE;       
 CREATE TABLE site2.userdata
 (
    user_id character varying(128) NOT NULL, 
@@ -61,7 +64,7 @@ CREATE TABLE site2.userdata
    city character varying(128) NOT NULL, 
    zip_code character varying(128) NOT NULL, 
    password character varying(32) NOT NULL, 
-   CONSTRAINT PRIMARY KEY (user_id)
+   PRIMARY KEY (user_id)
 ) 
 WITH (
   OIDS = FALSE
@@ -69,7 +72,7 @@ WITH (
 ALTER TABLE site2.userdata
   OWNER TO cmpe275;
   
-DROP TABLE IF EXISTS site2.image CASCADE    
+DROP TABLE IF EXISTS site2.image CASCADE; 
 CREATE TABLE site2.image
 (
    id serial NOT NULL, 
@@ -92,7 +95,7 @@ DROP SCHEMA IF EXISTS site3 CASCADE;
 CREATE SCHEMA site3
        AUTHORIZATION cmpe275;
        
-DROP TABLE IF EXISTS site3.userdata CASCADE       
+DROP TABLE IF EXISTS site3.userdata CASCADE;      
 CREATE TABLE site3.userdata
 (
    user_id character varying(128) NOT NULL, 
@@ -100,7 +103,7 @@ CREATE TABLE site3.userdata
    city character varying(128) NOT NULL, 
    zip_code character varying(128) NOT NULL, 
    password character varying(32) NOT NULL, 
-   CONSTRAINT PRIMARY KEY (user_id)
+   PRIMARY KEY (user_id)
 ) 
 WITH (
   OIDS = FALSE
@@ -108,7 +111,7 @@ WITH (
 ALTER TABLE site3.userdata
   OWNER TO cmpe275;
   
-DROP TABLE IF EXISTS site3.image CASCADE    
+DROP TABLE IF EXISTS site3.image CASCADE;   
 CREATE TABLE site3.image
 (
    id serial NOT NULL, 
@@ -131,7 +134,7 @@ DROP SCHEMA IF EXISTS site4 CASCADE;
 CREATE SCHEMA site4
        AUTHORIZATION cmpe275;
        
-DROP TABLE IF EXISTS site4.userdata CASCADE       
+DROP TABLE IF EXISTS site4.userdata CASCADE;      
 CREATE TABLE site4.userdata
 (
    user_id character varying(128) NOT NULL, 
@@ -139,7 +142,7 @@ CREATE TABLE site4.userdata
    city character varying(128) NOT NULL, 
    zip_code character varying(128) NOT NULL, 
    password character varying(32) NOT NULL, 
-   CONSTRAINT PRIMARY KEY (user_id)
+   PRIMARY KEY (user_id)
 ) 
 WITH (
   OIDS = FALSE
@@ -147,7 +150,7 @@ WITH (
 ALTER TABLE site4.userdata
   OWNER TO cmpe275;
   
-DROP TABLE IF EXISTS site4.image CASCADE    
+DROP TABLE IF EXISTS site4.image CASCADE;   
 CREATE TABLE site4.image
 (
    id serial NOT NULL, 
