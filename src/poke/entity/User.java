becfe5c6f639316849268adf12ab08f363b55548
sugandhi <sugandhi.sharma@gmail.com>
2013-03-31 04:46:31 -0700

    jpa changes

--- src/poke/entity/User.java
+++ src/poke/entity/User.java
@@ -1,5 +1,13 @@
 package poke.entity;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userdata")
 public class User {
 
 	private String userId;
@@ -8,30 +16,42 @@ public class User {
 	private String zipCode;
 	private String password;
 
+	@Id
+	@Basic(optional = false)
+	@Column(name = "id")
 	public String getUserId() {
 		return userId;
 	}
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+	
+	@Basic(optional = false)
+	@Column(name = "name")
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	@Basic(optional = false)
+	@Column(name = "city")
 	public String getCity() {
 		return city;
 	}
 	public void setCity(String city) {
 		this.city = city;
 	}
+	@Basic(optional = false)
+	@Column(name = "zip_code")
 	public String getZipCode() {
 		return zipCode;
 	}
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
+	@Basic(optional = false)
+	@Column(name = "password")
 	public String getPassword() {
 		return password;
 	}
